c79520aa4405e2e91e14d93ff01b64758353c568
Fernando Naufel <fnaufel@gmail.com>
2022-04-23 14:35:38 -0300

    Fix bhaskara header

--- Operando_com_vetores/bhaskara.R
+++ Operando_com_vetores/bhaskara.R
@@ -1,25 +1,4 @@
 
-# A idéia é substituir `...` pelos seus comandos.
-# 
-# Seja curioso. Digite `play()` no console e 
-# faça experiências no prompt do R.
-# 
-# Você pode pôr o cursor em qualquer comando neste script 
-# e teclar CTRL-Enter para executar o comando no console.
-# 
-# Ou você pode selecionar o trecho que você quiser 
-# e teclar CTRL-Enter para executar só esse trecho no console.
-# 
-# Use estes recursos para testar sua solução.
-# 
-# Salve este arquivo quantas vezes você quiser.
-# 
-# Quando quiser verificar sua solução, salve este arquivo
-# e digite `submit()` no console.
-# 
-# Para desistir, digite `skip()` no console.
-# (Mas vc não vai desistir, vai?)
-
 
 discriminante <- function(a, b, c) {
   
